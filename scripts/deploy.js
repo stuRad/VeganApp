25f0cddfc7933b2a262c3c478295167b94bd905b
stuRad <87615935+stuRad@users.noreply.github.com>
2021-09-04 15:57:29 -0400

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,21 +1,10 @@
-//const hre = require("hardhat");
-
 async function deployer() {
   const AnimalNFT = await hre.ethers.getContractFactory("AnimalNFT");
   const nft = await AnimalNFT.deploy();
 
   await nft.deployed();
 
-  //console.log("AnimalNFT deployed to:", nft.address);
   return nft.address;
 }
-
-// deployer()
-//   .then(() => process.exit(0))
-//   .catch((error) => {
-//     console.error(error);
-//     process.exit(1);
-//   });
-
 
 module.exports = {deployer};
