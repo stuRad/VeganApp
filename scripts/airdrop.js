1f9614a72a5a34a49f4a17375e0e8f95fcd2f911
stuRad <87615935+stuRad@users.noreply.github.com>
2021-09-07 14:42:17 -0400

    updated for accounts.js
    takes random wallet addresses from accounts.js and calculates time since going vegan then transfers tokens based on that. hardcoded for now. Can be verified on rinkeby.etherscan as transfers from your own address

--- scripts/airdrop.js
+++ scripts/airdrop.js
@@ -1,11 +1,21 @@
-const VeganBankAddress =   "0x86990A525c0E2b9eF30981288A5C3685cE5fCb30";
-const existingContractAddr = "0x361E25cA8a467541Fb45F6550E31bF3559161ACD";
+//const VeganBankAddress =   "0x86990A525c0E2b9eF30981288A5C3685cE5fCb30";
+const {addresses} = require("./accounts");
+
+//Run scripts/initialSupply.js then paste the address here
+const existingContractAddr = "";
 
 async function main() {
   const token = await hre.ethers.getContractAt("VEGANTOKEN", existingContractAddr);
 
-    const amount = ethers.utils.parseEther("80");
-    await token.transfer(VeganBankAddress, amount);
+  for (i = 0; i < addresses.length; i++) {
+
+    let daysVegan = Math.floor((((((Date.now() - addresses[i].veganniversary) / 1000) / 60) / 60) / 24));
+
+    //console.log(addresses[i].address);
+    await token.transfer(addresses[i].address, ethers.utils.parseEther(daysVegan.toString()));
+
+  }
+
 
 }
 
