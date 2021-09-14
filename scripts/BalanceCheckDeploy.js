ecae08944b9aa13f298ba3c95f11cb63e0ef1b08
chinchin48 <65033771+chinchin48@users.noreply.github.com>
2021-09-14 13:29:35 -0500

    Create BalanceCheckDeploy.js

---
+++ scripts/BalanceCheckDeploy.js
@@ -0,0 +1,20 @@
+async function balanceDeploy() {
+
+  const BalanceChecker = await ethers.getContractFactory("BalanceChecker");
+  const balCheck = await BalanceChecker.deploy();
+
+  await balCheck.deployed();
+
+  console.log("contract deployed to: " + balCheck.address);
+
+//return contract Address
+}
+
+balanceDeploy()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+module.exports = {balanceDeploy};
