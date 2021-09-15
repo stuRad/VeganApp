068e689f834d896798a737358504cd1383d87864
Stuart <rutty.stuart@gmail.com>
2021-09-15 14:36:20 -0400

    new account functions, need to modify all other scripts that call this to reflect

--- scripts/Accounts/accountCreation.js
+++ scripts/Accounts/accountCreation.js
@@ -11,11 +11,42 @@
 
 //console.log(addresses);
 
-let address = {
-  address: ethers.Wallet.createRandom().address,
-  veganniversary: new Date(2021, 4, 15),
-  accountCreated: new Date(),
-  lastLogin: new Date(2021, 4, 15)
-};
+function createMyAccount() {
+  return {
+    address: "0x6368CD30cc23277CF1886841E138e864e11d1981",
+    veganniversary: new Date(2019, 1, 18),
+    accountCreated: new Date(),
+    lastLogin: new Date(2021, 8, 1)
+  };
+}
 
-module.exports = {address}
+function createRandomAccount() {
+  const veganniversaryDate = randomDate(new Date(2017, 0, 1), new Date(2020, 0, 1));
+  const accountCreatedDate = randomDate(veganniversaryDate, new Date());
+  const lastLoginDate = randomDate(accountCreatedDate, new Date());
+  return {
+    address: ethers.Wallet.createRandom().address,
+    veganniversary: veganniversaryDate,
+    accountCreated: accountCreatedDate,
+    lastLogin: lastLoginDate
+  };
+}
+
+function createAccountWithParams(veganniversaryDate, accountCreatedDate, lastLoginDate) {
+  return {
+    address: ethers.Wallet.createRandom().address,
+    veganniversary: new Date(veganniversaryDate),
+    accountCreated: new Date(accountCreatedDate),
+    lastLogin: new Date(lastLoginDate)
+  };
+}
+
+function randomDate(start, end) {
+  return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+}
+
+
+const a = createRandomAccount();
+console.log(a);
+
+module.exports = {createMyAccount, createAccountWithParams}
