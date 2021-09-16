b33b3bdf191f2f0d5c9286d25afd9d0ff807f969
chinchin48 <65033771+chinchin48@users.noreply.github.com>
2021-09-15 23:20:09 -0500

    Update initialAirdropTests.js

--- test/initialAirdropTests.js
+++ test/initialAirdropTests.js
@@ -69,3 +69,49 @@ describe("Account with 30 days of being vegan", () => {
     assert.equal(nftCount, 1);
   }).timeout(35000);
 });
+
+describe("Reward", () => {
+    it("should mint 1 VGN token if a day has passed since previous lastLogin day", async () => {
+      //One Day Time in ms(milliseconds)
+       var oneDay = 1000*60*60*24;
+       var today = new Date();
+       var yesterday = new Date(today);
+       yesterday.setDate(yesterday.getDate()-1);
+       console.log(today);
+       console.log(yesterday);
+
+      //need to update these dates to be yesterday
+      var userOneDay = {
+       address: ethers.Wallet.createRandom().address,
+       veganniversary: new Date(yesterday),
+       accountCreated: new Date(yesterday),
+       lastLogin: new Date(yesterday)
+     };
+
+    var daysSinceLastLogin = Math.floor((today.getTime() - userOneDay.lastLogin.getTime()) / (1000 * 3600 * 24));
+
+
+        await handleReward(userOneDay);
+        const userAddress = userOneDay.address;
+        //give etherscan time to see + track the transction
+
+        await new Promise(function (resolve, reject) {
+          setTimeout(function () {
+            resolve("anything");
+          }, 30000);
+        });
+
+
+      const result = await api.account.tokenbalance(userAddress, '', VEGGIECOINS);
+      tokenCount = ethers.utils.formatEther(result.result);
+      console.log(tokencount);
+
+
+      assert.equal(tokenCount, daysSinceLastLogin);
+
+
+    })
+
+});
+
+
